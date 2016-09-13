61df9f0cd8707398e02cab47788809064661f891
Danika Harada <danikaharada@Danikas-MacBook-Air.local>
2016-09-13 13:55:09 -1000

    completed presidents for loop

--- loops.js
+++ loops.js
@@ -10,9 +10,14 @@ Within the _FOR_ loop, use `console.log` to:
 
 /***bonus:** could you take the code inside of this FOR loop and encapsulate it inside of a function called `printContent` and still achieve the same result? This new function should accept two parameters.*/
 
+var presidents = ["Washington", "Adams", "Jefferson", "Madison", "Monroe"];
 
+for (var i = 0; i < presidents.length; i++){
+	console.log(presidents[i]);
+	console.log('value at i is ', presidents[i]);
+	console.log('value at ', [i], ' is ', presidents[i]);
 
-
+}
 
 /*Declare a variable named `stringOfNumbers` and set it's value to `''` (an empty String).*/
 
